f72ca64a3b5cc154f2ce7cd96fef247cb1566cc3
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-02 02:28:04 -0800

    EventsActivity is now a LocationListener.

--- src/com/teamsexy/helloTabs/EventsActivity.java
+++ src/com/teamsexy/helloTabs/EventsActivity.java
@@ -1,15 +1,38 @@
 package com.teamsexy.helloTabs;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class EventsActivity extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
+public class EventsActivity extends Activity implements LocationListener {
+    
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         TextView textview = new TextView(this);
         textview.setText("This is the EVENTS tab");
         setContentView(textview);
     }
+
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
 }
