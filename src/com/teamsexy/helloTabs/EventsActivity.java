b1c1fa39d95c79ad8797be05bc67547d215a2547
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-02 02:50:41 -0800

    Filled in a few LocationListener methods as placeholder; added onLocationChange text notification for latitude/longitude.

--- src/com/teamsexy/helloTabs/EventsActivity.java
+++ src/com/teamsexy/helloTabs/EventsActivity.java
@@ -3,36 +3,56 @@ package com.teamsexy.helloTabs;
 import android.app.Activity;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class EventsActivity extends Activity implements LocationListener {
     
+	private LocationManager locationManager;
+	private LocationProvider locationProvider;
+	private TextView textview;
+	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        TextView textview = new TextView(this);
+        // Initialize TextView
+        textview = new TextView(this);
         textview.setText("This is the EVENTS tab");
         setContentView(textview);
+        
+        // Initialize LocationManager
+        
     }
+	
+	/* Activity behavior on startup / pause */
+	protected void onResume() {
+		super.onResume();
+		// State-retrieving activity here
+		// Perhaps a feed and/or location update
+	}
+	
+	protected void onPause() {
+		super.onPause();
+		// State-saving activity here
+	}
 
+	/* Location listening functionality */
 	public void onLocationChanged(Location location) {
-		// TODO Auto-generated method stub
+		int latitude = (int) (location.getLatitude());
+		int longitude = (int) (location.getLongitude());
 		
+		textview.setText("New Location: " + latitude + ", " + longitude);
+		setContentView(textview);
 	}
 
-	public void onProviderDisabled(String provider) {
-		// TODO Auto-generated method stub
-		
-	}
+	// TODO: Provider disabled behavior
+	public void onProviderDisabled(String provider) {}
 
-	public void onProviderEnabled(String provider) {
-		// TODO Auto-generated method stub
-		
-	}
+	// TODO: Provider enable behavior
+	public void onProviderEnabled(String provider) {}
 
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
-		
-	}
+	// TODO: Placeholder for now
+	public void onStatusChanged(String provider, int status, Bundle extras) {}
 }
