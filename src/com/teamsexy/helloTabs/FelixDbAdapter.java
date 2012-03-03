db6c8b653fdf1404cc11571657ff131d075827bf
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-02 21:55:52 -0800

    Initial database helper subclass in FelixDbAdapter, and a constructor.

--- src/com/teamsexy/helloTabs/FelixDbAdapter.java
+++ src/com/teamsexy/helloTabs/FelixDbAdapter.java
@@ -1,5 +1,56 @@
 package com.teamsexy.helloTabs;
 
-public class FelixDbAdapter {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
+/**
+ * FelixDbAdapter
+ * @author genia
+ * 
+ * Database adapter for Felix, the happy hour app. Provides basic CRUD
+ * functionality for spots, events, other user info.
+ *
+ * Inspired by Android Notepad Tutorial.
+ */
+public class FelixDbAdapter {
+	
+	private DatabaseHelper dbHelper;
+    private SQLiteDatabase db;
+    
+    /**
+     * Database creation sql statement
+     */
+    //private static final String DATABASE_CREATE =
+    //  "create table notes (_id integer primary key autoincrement, "
+    //    + "title text not null, body text not null);";
+
+    private static final String DATABASE_NAME = "felixdb";
+    private static final int DATABASE_VERSION = 2;
+    private final Context context;
+    
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+    	public DatabaseHelper(Context context) {
+    		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    	}
+		
+		@Override
+		public void onCreate(SQLiteDatabase arg0) {
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		}
+    }
+    
+    /**
+     * Constructor - takes context to allow database management.
+     */
+    public FelixDbAdapter (Context context) {
+    	this.context = context;
+    }
+    
 }
