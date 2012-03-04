fed30f2632782af83a8060a0747a943522d84ecd
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-03 18:10:14 -0800

    Created activities for the editing/creation of Spots, Events, Groups

---
+++ src/com/teamsexy/helloTabs/SpotEditActivity.java
@@ -0,0 +1,11 @@
+package com.teamsexy.helloTabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpotEditActivity extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+	}
+}
