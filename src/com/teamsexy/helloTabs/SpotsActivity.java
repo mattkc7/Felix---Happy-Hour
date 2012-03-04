02b5344065a860df82df4f1a46cf065a5268aaf9
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-03 21:17:44 -0800

    Cursor navigation for SpotsActivity creates a ListView in spots tab, if spots found, otherwise menu item lets you create a new one.

--- src/com/teamsexy/helloTabs/SpotsActivity.java
+++ src/com/teamsexy/helloTabs/SpotsActivity.java
@@ -1,19 +1,18 @@
 package com.teamsexy.helloTabs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.SimpleCursorAdapter;
 
 public class SpotsActivity extends ListActivity {
 
 	/* Spot database updates */
 	private FelixDbAdapter spotDbHelper;
-	
-	private static final String[] spotNames={"Mordor", 
-		"Rivendell", 
-		"The Shire",
-		"Isengard"};
 	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +23,6 @@ public class SpotsActivity extends ListActivity {
         
         // Fetch spots data
         getAllSpotsData();
-        
-        // Set list layout in Spots activity
-        setListAdapter(new ArrayAdapter<String>(this,
-        		android.R.layout.simple_list_item_1, spotNames));
-        
     }
     
     /**
@@ -38,5 +32,29 @@ public class SpotsActivity extends ListActivity {
      */
     public void getAllSpotsData() {    	
     	Cursor spotsCursor = spotDbHelper.getAllSpotEntries();
+    	List<String> spotNames = new ArrayList<String>();
+    	
+    	// If query returns spots, display them in Spots Tab
+    	// Might want to add ordering query so that most recent
+    	// spots display first...
+    	if (spotsCursor.getCount() > 0) {
+    		spotsCursor.moveToFirst();
+    		while (!spotsCursor.isAfterLast()) {
+    			spotNames.add(spotsCursor.getString(1));
+    			spotsCursor.moveToLast();
+    		}
+    		
+    	}
+    	
+    	//...otherwise if cursor is empty, display a "No Spots Found" message.
+    	// FIXME clicking on this should take one to the SpotsEdit activity
+    	else {
+    		spotNames.add("No spots found. Add one?");
+    	}
+ 
+    	// Close cursor
+    	spotsCursor.close();
+    	setListAdapter(new ArrayAdapter<String>(this, 
+				android.R.layout.simple_list_item_1, spotNames));
     }
 }
