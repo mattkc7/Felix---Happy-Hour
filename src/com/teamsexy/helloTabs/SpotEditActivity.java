8600788fe61b1340eb8d683bebe0ccc2f65a5d0e
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-04 21:44:52 -0800

    Updated SpotEditActivity as a placeholder view.

--- src/com/teamsexy/helloTabs/SpotEditActivity.java
+++ src/com/teamsexy/helloTabs/SpotEditActivity.java
@@ -3,14 +3,19 @@ package com.teamsexy.helloTabs;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SpotEditActivity extends ListActivity {
 
-	private static final String[] placeholder = {"Where Am I?", "Use My Location", "Add Location Later"};
+	private FelixDbAdapter spotDbHelper;
+	private ListView spoteditview;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.spoteditform);
         
+        // Initialize view
+        String[] placeholder = {"[Edit Spot Title]", "[Edit Spot About]", "[Location]"};
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, placeholder));
 	}
 }
