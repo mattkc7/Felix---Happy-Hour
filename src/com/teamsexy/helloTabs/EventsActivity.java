daa2d4651ed161b9aea53bd1955dda2ce31afb5d
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-06 11:07:27 -0800

    Changed EventsActivity to a FragmentActivity. No issues encountered.

--- src/com/teamsexy/helloTabs/EventsActivity.java
+++ src/com/teamsexy/helloTabs/EventsActivity.java
@@ -1,14 +1,14 @@
 package com.teamsexy.helloTabs;
 
-import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 
-public class EventsActivity extends Activity implements LocationListener {
+public class EventsActivity extends FragmentActivity implements LocationListener {
     
 	private LocationManager locationManager;
 	private String locationProvider;
