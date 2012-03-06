5db4e9eb53e72c480613544742808b41cb7c73b8
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-06 08:10:28 -0800

    Created basic spot editor view with a couple of text inputs, added to spotsEditActivity.

--- src/com/teamsexy/helloTabs/SpotEditActivity.java
+++ src/com/teamsexy/helloTabs/SpotEditActivity.java
@@ -1,21 +1,53 @@
 package com.teamsexy.helloTabs;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class SpotEditActivity extends ListActivity {
+public class SpotEditActivity extends Activity {
 
 	private FelixDbAdapter spotDbHelper;
 	private ListView spoteditview;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.spoteditform);
+        setContentView(R.layout.spoteditform);
+        
+        final EditText edittext1 = (EditText) findViewById(R.id.spottitle);
+        edittext1.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                  // Perform action on key press
+                  Toast.makeText(SpotEditActivity.this, edittext1.getText(), Toast.LENGTH_SHORT).show();
+                  return true;
+                }
+                return false;
+            }
+        });
+        
+        final EditText edittext2 = (EditText) findViewById(R.id.spotbody);
+        edittext2.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                  // Perform action on key press
+                  Toast.makeText(SpotEditActivity.this, edittext2.getText(), Toast.LENGTH_SHORT).show();
+                  return true;
+                }
+                return false;
+            }
+        });
         
         // Initialize view
-        String[] placeholder = {"[Edit Spot Title]", "[Edit Spot About]", "[Location]"};
-        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, placeholder));
+        //String[] placeholder = {"[Edit Spot Title]", "[Edit Spot About]", "[Location]"};
+        //setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, placeholder));
 	}
 }
