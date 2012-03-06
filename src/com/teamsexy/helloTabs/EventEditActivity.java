8b5c2bc7eefb33891e4f9696d92ee53a40638e61
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-05 22:49:37 -0800

    Added a placeholder list of values to EventEdit Activity.

--- src/com/teamsexy/helloTabs/EventEditActivity.java
+++ src/com/teamsexy/helloTabs/EventEditActivity.java
@@ -1,11 +1,19 @@
 package com.teamsexy.helloTabs;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class EventEditActivity extends Activity {
+public class EventEditActivity extends ListActivity {
 
+	private FelixDbAdapter eventDbHelper;
+	private ListView eventeditview;
+	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        String[] placeholder = {"[Edit Event Title]", "[Edit Event About]", "[Event Location]", "[Event Date/Time]"};
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, placeholder));
 	}
 }
