bf81348849ef2c405ef4327a75fb03959b13dd88
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-05 23:31:26 -0800

    Updated listview placeholders for Event / Group editors.

--- src/com/teamsexy/helloTabs/GroupEditActivity.java
+++ src/com/teamsexy/helloTabs/GroupEditActivity.java
@@ -1,12 +1,20 @@
 package com.teamsexy.helloTabs;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class GroupEditActivity extends Activity {
+public class GroupEditActivity extends ListActivity {
+	
+	private FelixDbAdapter groupDbHelper;
+	private ListView groupeditview;
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        String[] placeholder = {"[Group Name]", "[Group About]", "[Other TBD]"}; 
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, placeholder));
 	}
 
 }
