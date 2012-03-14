6083a893110e583bdf763b281c33f3b5a2459c29
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-14 09:18:22 -0700

    A couple small changes to get it running.

--- src/com/teamsexy/helloTabs/DetailForm.java
+++ src/com/teamsexy/helloTabs/DetailForm.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 
 public class DetailForm extends Activity { 
 	
