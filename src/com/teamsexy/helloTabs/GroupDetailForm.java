fe9b78518523d8967a8593cc0989c1e946cf4657
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-14 09:40:37 -0700

    Contact add button in Groups.

--- src/com/teamsexy/helloTabs/GroupDetailForm.java
+++ src/com/teamsexy/helloTabs/GroupDetailForm.java
@@ -1,13 +1,14 @@
 package com.teamsexy.helloTabs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
 public class GroupDetailForm extends Activity { 
 	
@@ -29,6 +30,8 @@ public class GroupDetailForm extends Activity {
         
         Button save = (Button)findViewById(R.id.save);
         save.setOnClickListener(onSave);
+        Button addFriends = (Button)findViewById(R.id.addFriends);
+        save.setOnClickListener(onAddFriends);
         
         spotId = getIntent().getStringExtra(GroupsActivity.ID_EXTRA);
         
@@ -82,4 +85,30 @@ public class GroupDetailForm extends Activity {
 			finish();
 		}
 	};
+	
+	private View.OnClickListener onAddFriends = new View.OnClickListener() {
+		public void onClick(View v) {
+			
+			startActivityForResult(new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI), 1);
+		}
+	};
+	
+	@Override
+	public void onActivityResult(int reqCode, int resultCode, Intent data) {
+	  super.onActivityResult(reqCode, resultCode, data);
+
+	  switch (reqCode) {
+	    case (1) :
+	      if (resultCode == Activity.RESULT_OK) {
+	        Uri contactData = data.getData();
+	        Cursor c =  managedQuery(contactData, null, null, null, null);
+	        if (c.moveToFirst()) {
+	          String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+	          
+	          // TODO STUFF with contacts
+	        }
+	      }
+	      break;
+	  }
+	}
 }
