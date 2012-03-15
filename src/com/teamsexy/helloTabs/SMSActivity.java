964e874100825452c65262e4c36c469fc2e5dfcd
Eugenia Gabrielova <genia.likes.science@gmail.com>
2012-03-14 20:04:14 -0700

    Created an SMS Activity for testing notifications.

---
+++ src/com/teamsexy/helloTabs/SMSActivity.java
@@ -0,0 +1,16 @@
+package com.teamsexy.helloTabs;
+
+import android.app.Activity;
+
+/**
+ * SMS Activity - SMS Messaging Notifications
+ * @author genia
+ *
+ * This activity is designed to send notifications by Text to some given number.
+ * It can be intiated from another activity as follows:
+ * 
+ */
+public class SMSActivity extends Activity {
+	
+
+}
